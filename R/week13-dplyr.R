3880e21d2e1105ed900f1850124d56dbda5e4d73
Vivien Lee <vivienlvv@gmail.com>
2023-04-27 09:05:17 -0500

    Part 1 Step 3 complete

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,5 +1,39 @@
 # Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(keyring)
+library(RMariaDB)
+
+
 # Data Import and Cleaning
-# Visualization
+
+## Setting key
+# key_set_with_value(service = "latis-mysql",
+#                    username = "username",
+#                    password = "password")
+
+## Getting data "week13.csv" from LATIS for the project 
+### I commented out the code after downloading the file 
+# connection <- dbConnect(MariaDB(),
+#                   user="lee02903",
+#                   password=key_get("latis-mysql","lee02903"),
+#                   host="mysql-prod5.oit.umn.edu",
+#                   port=3306,
+#                   ssl.ca = 'mysql_hotel_umn_20220728_interm.cer'
+# )
+# 
+# databases_df <- dbGetQuery(connection, "SHOW DATABASES;")
+# dbExecute(connection, "USE cla_tntlab;")
+# dbGetQuery(connection, "SHOW TABLES;")
+# datascience_8960_table = dbGetQuery(connection, "SELECT * FROM datascience_8960_table")
+# write_csv(datascience_8960_table, "../data/week13.csv")
+
+## Reading in created dataset 
+week13_tbl = read_csv("../data/week13.csv", show_col_types = FALSE)
+
 # Analysis
+
+
+
+
 # Publication
